6995faf43a15f2e69718ede72eac84f3ed2ed509
dorperets <dor.perets@gmail.com>
2023-10-05 09:24:06 +0300

    add description button version 1, on a textArea currently. add Strings.js

---
+++ react-chrome-app/res/strings.js
@@ -0,0 +1,8 @@
+const strings = {
+    all: {
+        description: ' <START_CODE_SIZE>\n1\n<END_CODE_SIZE>\n<START_SOURCE_CREATION>\nNo\n<END_SOURCE_CREATION>\n<START_UT_RESULTS>\nPASS (0 Tests Ran, 0 Tests Failed)\n<END_UT_RESULTS>\n<START_MANIFEST_UPDATES>\n<END_MANIFEST_UPDATES>\n<START_PROPOSED_NOTES>\n<END_PROPOSED_NOTES>'
+    }
+
+}
+
+export default strings
